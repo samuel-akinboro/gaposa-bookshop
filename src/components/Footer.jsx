5c42bea810087064ca1b6a9fc01b480d9e9d0142
samuel-akinboro <victorakinboro2003@gmail.com>
2024-07-06 11:40:39 +0100

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,10 +7,10 @@ const Footer = () => {
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between">
           <p>&copy; 2024 GAPOSA Bookshop. All rights reserved.</p>
-          <div>
+          {/* <div>
             <a href="#" className="text-gray-300 hover:text-gray-100 px-3 py-2">Privacy Policy</a>
             <a href="#" className="text-gray-300 hover:text-gray-100 px-3 py-2">Terms of Service</a>
-          </div>
+          </div> */}
         </div>
       </div>
     </footer>
